90b91f18ee28e42fb93226c341b5a315f53301fc
CTRLTry <ctrltry@gmail.com>
2025-05-29 01:06:09 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const clients = new Set();
+
+wss.on('connection', (ws) => {
+  clients.add(ws);
+
+  ws.on('message', (message) => {
+    for (const client of clients) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    }
+  });
+
+  ws.on('close', () => {
+    clients.delete(ws);
+  });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+server.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000');
+});
